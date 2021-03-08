f31d01a115be36fdc7a7fb5996e74e5f31bf01b5
Danny <danny.bergeron@hotmail.com>
2021-03-08 15:13:46 -0500

    refactor run_star

--- scripts/run_star.py
+++ scripts/run_star.py
@@ -1,4 +1,5 @@
 import subprocess
+
 input = snakemake.input
 output = snakemake.output
 params = snakemake.params
@@ -7,28 +8,27 @@ log = snakemake.log
 id = snakemake.wildcards.id
 
 
-# TODO: Refactor !?
-
 def run_star(index_dir,input1,input2,threads,outdir,stringence,log,zcat):
 
-  Star_list = ['STAR', '--runMode', 'alignReads',
-          '--genomeDir', f'{index_dir}',
-          '--readFilesIn', f'{input1}', f'{input2}',
-          '--runThreadN', f'{threads}',
-          '--readFilesCommand', 'zcat',
-          '--outReadsUnmapped', 'Fastx',
-          '--outFilterType', 'BySJout',
-          '--outStd', 'Log',
-          '--outSAMunmapped', 'None',
-          '--outSAMtype', 'BAM', 'SortedByCoordinate',
-
-          '--outFileNamePrefix', f'{outdir}',
-          '--outFilterScoreMinOverLread', '0.3',
-          '--outFilterMatchNminOverLread', '0.3',
-          '--outFilterMultimapNmax', '100',
-          '--winAnchorMultimapNmax', '100',
-          '--alignEndsProtrude', f'{stringence}', 'ConcordantPair',
-          '&>', f'{log}']
+  Star_list = [
+    'STAR', '--runMode', 'alignReads',
+    '--genomeDir', f'{index_dir}',
+    '--readFilesIn', f'{input1}', f'{input2}',
+    '--runThreadN', f'{threads}',
+    '--readFilesCommand', 'zcat',
+    '--outReadsUnmapped', 'Fastx',
+    '--outFilterType', 'BySJout',
+    '--outStd', 'Log',
+    '--outSAMunmapped', 'None',
+    '--outSAMtype', 'BAM', 'SortedByCoordinate',
+    '--outFileNamePrefix', f'{outdir}',
+    '--outFilterScoreMinOverLread', '0.3',
+    '--outFilterMatchNminOverLread', '0.3',
+    '--outFilterMultimapNmax', '100',
+    '--winAnchorMultimapNmax', '100',
+    '--alignEndsProtrude', f'{stringence}', 'ConcordantPair',
+    '&>', f'{log}'
+  ]
 
   if zcat :
     del Star_list[10]
@@ -37,23 +37,27 @@ def run_star(index_dir,input1,input2,threads,outdir,stringence,log,zcat):
   subprocess.call(Star_list)
 
 # Run the first time
-run_star(f"{params.index_dir}",
-         f"{input.fastq1}",
-         f"{input.fastq2}",
-         f"{threads}",
-         f"{params.outdir0}",
-         0,
-         f"{log}",
-         False)
+run_star(
+  f"{params.index_dir}",
+  f"{input.fastq1}",
+  f"{input.fastq2}",
+  f"{threads}",
+  f"{params.outdir0}",
+  0,
+  f"{log}",
+  False
+)
 
 # Take the Unmapped for other rounds
 for i in range(1,4):
 
-  run_star(f"{params.index_dir}",
-           f"{params.outdir2}{i-1}/{id}/Unmapped.out.mate1",
-           f"{params.outdir2}{i-1}/{id}/Unmapped.out.mate2",
-           f"{threads}",
-           f"{params.outdir2}{i}/{id}/",
-           i,
-           f"{log}{i}/{id}/",
-           True)
+  run_star(
+    f"{params.index_dir}",
+    f"{params.outdir2}{i-1}/{id}/Unmapped.out.mate1",
+    f"{params.outdir2}{i-1}/{id}/Unmapped.out.mate2",
+    f"{threads}",
+    f"{params.outdir2}{i}/{id}/",
+    i,
+    f"{log}{i}/{id}/",
+    True
+  )
